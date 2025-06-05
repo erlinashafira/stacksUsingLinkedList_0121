2f48fa0ba44bf5f1f2be57650192a6558330c6bf
errerew <erlina31acc@gmail.com>
2025-06-05 14:43:49 +0700

    menambahkan class stack

--- stecks.cpp
+++ stecks.cpp
@@ -5,4 +5,19 @@ class Node{
     public:
     int data;
     Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
+
+class stack
+{
+    private:
+    Node *top;
+    public:
+    stack(){
+        top = NULL;
+    }
 }
