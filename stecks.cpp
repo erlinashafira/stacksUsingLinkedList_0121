88d17fcbc095d5550b4e35e9c95d5b31a499731f
errerew <erlina31acc@gmail.com>
2025-06-05 15:09:59 +0700

    menambahkan fungsi main memilih 4 pilihan

--- stecks.cpp
+++ stecks.cpp
@@ -63,3 +63,19 @@ class stack
         }
     }
 };
+    int main(){
+        stack stack;
+
+        int choice = 0;
+        int value;
+
+        while (choice != 4)
+        {
+            cout << "1. push\n";
+            cout << "2. pop\n";
+            cout << "3. peek\n";
+            cout << "4. exit\n";
+            cout << "enter your choice: ";
+            cin >> choice;
+        }
+    };
